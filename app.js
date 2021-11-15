ef84e5354e2e0b7a0a52aef2b11114b220d11224
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-11-15 13:04:45 -0300

    fechar modal e adicao de item no carrinho

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
 const vm = new Vue({
   el: "#app",
+
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    carrinho: [],
   },
+
   filters: {
     precoFormat(valor) {
       return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" })
     }
   },
+
+  computed: {
+    carrinhoTotal() {
+      let total = 0
+
+      if (this.carrinho.length > 0) {
+        this.carrinho.forEach(item => {
+          total += item.preco
+        })
+      }
+
+      return total
+    },
+  },
+
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
         .then(r => r.json())
         .then(r => this.produtos = r)
     },
+
     fecthProduto(id) {
       fetch(`./api/produtos/${id}/dados.json`)
         .then(r => r.json())
         .then(r => this.produto = r)
-    }
+    },
+
+    abrirProduto(id) {
+      this.fecthProduto(id)
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    },
+
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false
+    },
+
+    adicionarItem() {
+      if (this.produto.estoque > 0) {
+        const { id, nome, preco } = this.produto
+
+        this.produto.estoque--
+        this.carrinho.push({ id, nome, preco })
+      }
+    },
+
+    removerItem(index) {
+      if (this.carrinho.length > 0) {
+        this.carrinho.splice(index, 1)
+      }
+    },
   },
+
   created() {
     this.fetchProdutos()
   }
